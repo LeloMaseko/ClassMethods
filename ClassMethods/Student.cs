5bd69892ebc172c6d568d69b284fc3286f0a2819
A253560 <A253560@LSBSA-CCTCSV3>
2023-12-05 17:12:55 +0200

    Add project files.

---
+++ ClassMethods/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods
+{
+    internal class Student
+    {
+        public double weight;
+        public string name;
+        public int age;
+
+        public Student(double aWeight, string aName, int aAge)
+        {
+            this.weight = aWeight;
+            this.name = aName;
+            this.age = aAge;
+        }
+        public bool IsObese() 
+        {
+            if (weight > 60)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
